6b0d9a2bb4d89ba0f20b6ed016b90a39a98f0493
Lenovo <artyomkuteynikov4@gmail.com>
2024-04-04 14:48:05 +0300

    Added chat module

---
+++ backoffice/сhat/admin.py
@@ -0,0 +1,120 @@
+import datetime
+import random
+import string
+from django.contrib.auth.admin import UserAdmin
+from django.contrib import admin
+from django.contrib.auth.models import User
+from django.db.models import Count, Sum, F
+from django.utils.safestring import mark_safe
+
+from .models import Settings, Customer, Transactions, Subscriptions
+
+
+class SubscriptionsInline(admin.TabularInline):
+    model = Subscriptions
+    extra = 0
+
+    def has_change_permission(self, request, obj=None):
+        return False
+
+    def has_add_permission(self, request, obj):
+        return False
+
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+
+@admin.register(Transactions)
+class TransactionsAdmin(admin.ModelAdmin):
+    list_filter = ['type', 'method', 'paid', 'created']
+    ordering = ['-created']
+    list_display = ['customer', 'amount', 'type', 'method', 'tokens', 'paid', 'created']
+
+    def has_change_permission(self, request):
+        return False
+
+    def has_add_permission(self, request):
+        return False
+
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+
+@admin.register(Settings)
+class SettingsAdmin(admin.ModelAdmin):
+    fieldsets = (
+        ('Настройки подписки', {
+            'fields': ('subscription_price', 'token_price', 'tokens_in_subscription', 'text'),
+        }),
+        ('Настройки реферальной системы', {
+            'fields': ('max_refers', 'referer_tokens', ),
+        })
+    )
+
+    def has_add_permission(self, request):
+        if Settings.objects.all():
+            return False
+        return True
+
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+
+class StaffAdmin(UserAdmin):
+    def get_queryset(self, request):
+        return super().get_queryset(request).filter(is_staff=True)
+
+    def save_model(self, request, obj, form, change):
+        obj.is_staff = True
+        super().save_model(request, obj, form, change)
+
+
+@admin.register(Customer)
+class EmployeeAdmin(admin.ModelAdmin):
+
+    exclude = ['first_name', 'last_name', 'last_login', 'username', 'is_superuser', 'is_staff', 'groups', 'user_permissions', 'date_joined', 'uuid', 'show', 'password']
+    list_display = ['edit', 'name', 'surname', 'email']
+
+    actions = ['delete', 'invite']
+
+    inlines = [SubscriptionsInline]
+
+    def delete(self, request, queryset):
+        for courier in queryset:
+            courier.show = False
+            courier.save()
+
+    delete.short_description = 'Удалить'
+
+    def invite(self, request, queryset):
+        for employee in queryset:
+            if not employee.password:
+                employee.confirm_email()
+
+    invite.short_description = 'Повторно отправить подтверждение'
+
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+    def get_queryset(self, request):
+        return super().get_queryset(request).filter(is_staff=False, show=True)
+
+    def save_model(self, request, obj, form, change):
+        if not change:
+            obj.set_password_hash()
+        obj.is_staff = False
+        super().save_model(request, obj, form, change)
+
+    def edit(self, obj):
+        return mark_safe('<i class="fa-solid fa-pen fa-fw"></i>')
+
+    def view(self, obj):
+        return mark_safe('<i class="fa-solid fa-eye"></i>')
+
+    edit.short_description = ''
+    view.short_description = ''
+
+
+admin.site.unregister(User)
+admin.site.register(User, StaffAdmin)
+
---
+++ backoffice/сhat/models.py
@@ -0,0 +1,19 @@
+from ckeditor.fields import RichTextField
+from django.db import models
+from passlib.context import CryptContext
+
+from system.models import Customer
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+
+class Chat(models.Model):
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE, verbose_name="Клиент")
+
+    class Meta:
+        ordering = ('id',)
+        verbose_name = 'Чат'
+        verbose_name_plural = 'Чаты'
+
+    def __str__(self):
+        return f'Чат {self.id}'
