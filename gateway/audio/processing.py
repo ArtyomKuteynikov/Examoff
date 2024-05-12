a40f1de381b934f4bd726ce1e73b1601f1871a38
Lenovo <artyomkuteynikov4@gmail.com>
2024-05-13 00:39:10 +0300

    connected voice processing to api

--- gateway/audio/processing.py
+++ gateway/audio/processing.py
@@ -7,13 +7,13 @@ import os
 
 
 def processing(test_audio, voice):
-    combined_filename = f'audio-temp/{uuid.uuid4()}.wav'
+    combined_filename = f'{os.getcwd()}/audio-temp/{uuid.uuid4()}.wav'
     cmd = f"ffmpeg -i {test_audio} -i {voice} -filter_complex concat=n=2:v=0:a=1 {combined_filename}"
     os.system(cmd)
     professor = diarizer(combined_filename)
     transcription = None
     if professor:
-        fixed_filename = f'audio-temp/{uuid.uuid4()}.wav'
+        fixed_filename = f'{os.getcwd()}/audio-temp/{uuid.uuid4()}.wav'
         quite = f'{os.getcwd()}/gateway/audio/audio_files/quite.wav'
         cmd = f"ffmpeg -i {quite} -i {voice} -filter_complex concat=n=2:v=0:a=1 {fixed_filename}"
         os.system(cmd)
