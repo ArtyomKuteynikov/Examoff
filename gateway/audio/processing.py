d5648d9e5619bfa8cc6dd5e6f6f2a86c0883fdab
Lenovo <artyomkuteynikov4@gmail.com>
2024-05-13 01:04:59 +0300

    connected voice processing to api

--- gateway/audio/processing.py
+++ gateway/audio/processing.py
@@ -7,19 +7,23 @@ import os
 
 
 def processing(test_audio, voice):
-    combined_filename = f'{os.getcwd()}/audio-temp/{uuid.uuid4()}.wav'
-    cmd = f"ffmpeg -i {test_audio} -i {voice} -filter_complex concat=n=2:v=0:a=1 {combined_filename}"
-    os.system(cmd)
-    professor = diarizer(combined_filename)
-    transcription = None
-    if professor:
-        fixed_filename = f'{os.getcwd()}/audio-temp/{uuid.uuid4()}.wav'
-        quite = f'{os.getcwd()}/gateway/audio/audio_files/quite.wav'
-        cmd = f"ffmpeg -i {quite} -i {voice} -filter_complex concat=n=2:v=0:a=1 {fixed_filename}"
-        os.system(cmd)
-        transcription = run(fixed_filename)
-        if os.path.exists(fixed_filename):
-            os.remove(fixed_filename)
-    if os.path.exists(combined_filename):
-        os.remove(combined_filename)
-    return transcription
+    try:
+        combined_filename = f'{os.getcwd()}/audio-temp/{uuid.uuid4()}.wav'
+        cmd = f"ffmpeg -i {test_audio} -i {voice} -filter_complex concat=n=2:v=0:a=1 {combined_filename}"
+        print(os.system(cmd))
+        professor = diarizer(combined_filename)
+        transcription = None
+        if professor:
+            fixed_filename = f'{os.getcwd()}/audio-temp/{uuid.uuid4()}.wav'
+            quite = f'{os.getcwd()}/gateway/audio/audio_files/quite.wav'
+            cmd = f"ffmpeg -i {quite} -i {voice} -filter_complex concat=n=2:v=0:a=1 {fixed_filename}"
+            os.system(cmd)
+            transcription = run(fixed_filename)
+            if os.path.exists(fixed_filename):
+                os.remove(fixed_filename)
+        if os.path.exists(combined_filename):
+            os.remove(combined_filename)
+        return transcription
+    except Exception as e:
+        print('ERROR:', e, test_audio, voice, combined_filename)
+        return None
